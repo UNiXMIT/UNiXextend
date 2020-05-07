216054bd3c1206f79bb8aff5f9b609a2c8fe8126
Martin Turner <4117073+UNiXMIT@users.noreply.github.com>
2020-05-07 09:43:36 +0100

    Update for multi-versions

--- linux/AcuScripts/startacurcl.sh
+++ linux/AcuScripts/startacurcl.sh
@@ -3,11 +3,12 @@
 # Make sure you have run that script first before running this one.
 
 ACURCL_PORT=$1
+SERVER_ALIAS_FILE=/home/support/AcuSupport/etc/acurcl.ini
 
 if [ -z "$ACURCL_PORT" ] ; then
+        ACCESS_FILE=/home/support/AcuSupport/etc/AcuAccess
         $ACUCOBOL/bin/acurcl -start -@
 else
         ACCESS_FILE=/home/support/AcuSupport/etc/AcuAccess$1
-        SERVER_ALIAS_FILE=/home/support/AcuSupport/etc/acurcl.ini
         $ACUCOBOL/bin/acurcl -start -n $ACURCL_PORT -@
 fi
