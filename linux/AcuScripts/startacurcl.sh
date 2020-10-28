505d8de26dbd5443319a3f28f000c938b86557c2
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2020-10-28 11:35:16 +0000

    Improvements

--- linux/AcuScripts/startacurcl.sh
+++ linux/AcuScripts/startacurcl.sh
@@ -3,26 +3,26 @@
 # Make sure you have run that script first before running this one.
 
 ACURCL_PORT=$1
-ACULOG=$2
+ACUOPT=$2
 export ACUSUP=/home/support/AcuSupport
 export SERVER_ALIAS_FILE=$ACUSUP/etc/acurcl.ini
 export FILE_TRACE_TIMESTAMP=TRUE
 
 if [ -z "$ACURCL_PORT" ] ; then
-        if [ -z $ACULOG ] ; then
-                export ACCESS_FILE=$ACUSUP/etc/AcuAccess
-                $ACUCOBOL/bin/acurcl -start -@
-        else
+        if [ $ACUOPT = log ] ; then
                 export ACCESS_FILE=$ACUSUP/etc/AcuAccess
                 $ACUCOBOL/bin/acurcl -start -le $ACUSUP/AcuLogs/acurcl.log -t7 -@
+        else
+                export ACCESS_FILE=$ACUSUP/etc/AcuAccess
+                $ACUCOBOL/bin/acurcl -start -@
         fi
 else
-        if [ -z $ACULOG ] ; then
-                export ACCESS_FILE=$ACUSUP/etc/AcuAccess$ACURCL_PORT
-                $ACUCOBOL/bin/acurcl -start -n $ACURCL_PORT -@
-        else
+        if [ $ACUOPT = log ] ; then
                 export ACCESS_FILE=$ACUSUP/etc/AcuAccess$ACURCL_PORT
                 $ACUCOBOL/bin/acurcl -start -n $ACURCL_PORT -le $ACUSUP/AcuLogs/$ACURCL_PORT-acurcl.log -t7 -@
+        else
+                export ACCESS_FILE=$ACUSUP/etc/AcuAccess$ACURCL_PORT
+                $ACUCOBOL/bin/acurcl -start -n $ACURCL_PORT -@
         fi
 fi
 
