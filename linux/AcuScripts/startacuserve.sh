c842acad94024f7a7260f47c2953f05b0277902c
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2020-11-18 16:50:29 +0000

    Updates

--- linux/AcuScripts/startacuserve.sh
+++ linux/AcuScripts/startacuserve.sh
@@ -2,12 +2,12 @@
 # The ACUCOBOL variable used in this script is set in the setenv.sh script.
 # Make sure you have run that script first before running this one.
 
-ACUSERVE_PORT=$1
 ACUOPT=$2
 ACUSUP=/home/support/AcuSupport
+export ACUSERVER_PORT=$1
 export FILE_TRACE_TIMESTAMP=TRUE
 
-if [[ -z "$ACUSERVE_PORT" ]] ; then
+if [[ -z "$ACUSERVER_PORT" ]] ; then
         if [[ "$ACUOPT" = "log" ]] ; then
                 export ACCESS_FILE=$ACUSUP/etc/AcuAccess
                 $ACUCOBOL/bin/acuserve -start -c $ACUSUP/etc/a_srvcfg -le $ACUSUP/AcuLogs/acuserve.log -t7 -@
@@ -17,10 +17,10 @@ if [[ -z "$ACUSERVE_PORT" ]] ; then
         fi
 else
         if [[ "$ACUOPT" = "log" ]] ; then
-                export ACCESS_FILE=$ACUSUP/etc/AcuAccess$ACUSERVE_PORT
-                $ACUCOBOL/bin/acuserve -start -c $ACUSUP/etc/a_srvcfg -n $ACUSERVE_PORT -le $ACUSUP/AcuLogs/$ACUSERVE_PORT-acuserve.log -t7 -@
+                export ACCESS_FILE=$ACUSUP/etc/AcuAccess$ACUSERVER_PORT
+                $ACUCOBOL/bin/acuserve -start -c $ACUSUP/etc/a_srvcfg -n $ACUSERVER_PORT -le $ACUSUP/AcuLogs/$ACUSERVER_PORT-acuserve.log -t7 -@
         else
-                export ACCESS_FILE=$ACUSUP/etc/AcuAccess$ACUSERVE_PORT
-                $ACUCOBOL/bin/acuserve -start -c $ACUSUP/etc/a_srvcfg -n $ACUSERVE_PORT -@
+                export ACCESS_FILE=$ACUSUP/etc/AcuAccess$ACUSERVER_PORT
+                $ACUCOBOL/bin/acuserve -start -c $ACUSUP/etc/a_srvcfg -n $ACUSERVER_PORT -@
         fi
 fi
