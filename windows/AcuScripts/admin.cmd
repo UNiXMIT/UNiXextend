5a78da2f2c32b4a611b5caf64e261c8b87488f9d
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2020-11-23 10:27:05 +0000

    Updates

--- windows/AcuScripts/admin.cmd
+++ windows/AcuScripts/admin.cmd
@@ -3,7 +3,15 @@
 SET EXTEND=%1
 IF '%1'=='' GOTO END
 
+:ACURCL
+reg query "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Micro Focus\extend %EXTEND%\AcuGT\bin\acurcl.exe" >nul 2>&1
+IF %ERRORLEVEL% == 0 GOTO ATW
 reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Micro Focus\extend %EXTEND%\AcuGT\bin\acurcl.exe" /t REG_SZ /d "~ RUNASADMIN" /f
+GOTO ATW
+
+:ATW
+reg query "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Micro Focus\extend %EXTEND%\AcuGT\bin\AcuToWeb.exe" >nul 2>&1
+IF %ERRORLEVEL% == 0 GOTO END
 reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Micro Focus\extend %EXTEND%\AcuGT\bin\AcuToWeb.exe" /t REG_SZ /d "~ RUNASADMIN" /f
 GOTO END
 
