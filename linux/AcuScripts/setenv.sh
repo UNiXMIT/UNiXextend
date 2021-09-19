59566ee93de20472365abf0f82f5a06692d9f568
Martin Turner <4117073+UNiXMIT@users.noreply.github.com>
2021-09-19 08:13:37 +0100

    Update setenv.sh

--- linux/AcuScripts/setenv.sh
+++ linux/AcuScripts/setenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# The startacu.sh script run after this, to start/stop services will use the ACUCOBOL variable set here.
+# The startacu.sh script run after this, to start/stop services, will use the ACUCOBOL variable set here.
 # ACU_OPT is the location of all your Acu installations
 export ACU_OPT=/home/products
 # Set the path to other installations here:
