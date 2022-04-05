9ee2730ad3fa3eea405e821b9e9ca2a5aa4ae5fd
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-04-05 20:43:54 +0100

    Updates

--- linux/etc/fillCombo.js
+++ linux/etc/fillCombo.js
@@ -2,6 +2,6 @@
 {"type": "category", "text": "Sample Applications"},
 {"type": "elem", "text": "Vision", "value": "Vision"},
 {"type": "elem", "text": "Tour", "value": "Tour"},
-{"type": "elem", "text": "CCOPY", "value": "ccopy"},
+{"type": "elem", "text": "ATWD", "value": "AcuToWeb Desktop Check"},
 {"type": "category", "text": "Customer Applications"}
 ]
--- windows/etc/fillCombo.js
+++ windows/etc/fillCombo.js
@@ -2,7 +2,7 @@
     {"type": "category", "text": "Sample Applications"},
     {"type": "elem", "text": "Vision", "value": "Vision"},
     {"type": "elem", "text": "Tour", "value": "Tour"},
-    {"type": "elem", "text": "CCOPY", "value": "ccopy"},
+    {"type": "elem", "text": "ATWD", "value": "AcuToWeb Desktop Check"},
     {"type": "category", "text": "Customer Applications"}
     ]
     
