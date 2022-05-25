915cd96e099a577e4019846bf3963fd6366df15d
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-05-25 11:32:58 +0100

    Version check

--- linux/AcuScripts/version.sh
+++ linux/AcuScripts/version.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-export ACUCOBOL=$1
-export SHARED=$ACUCOBOL/lib/libruncbl64.so
-if [ -z "$ACUCOBOL" ]
+# EXAMPLE USAGE: ./version.sh /home/products/acu1041stx64/
+export ACUCHECK=$1
+if [ -z "$ACUCHECK" ]
 then
     echo "Enter root directory of the AcuCOBOL installation: "
-    read x
+    read ACUCHECK
 fi
+export SHARED=$ACUCHECK/lib/libruncbl64.so
 if [ -f "$SHARED" ];
 then
     echo "You have installed the SHARED version of AcuCOBOL"
