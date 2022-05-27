74480b1605596a491e9646af539105f46d2e4cd9
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-05-27 17:02:15 +0100

    Consistent formatting

--- windows/AcuScripts/ATWD.cmd
+++ windows/AcuScripts/ATWD.cmd
@@ -4,12 +4,12 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome" /v InsecurePrivateN
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome\InsecurePrivateNetworkRequestsAllowedForUrls" /v 1 /t REG_SZ /d  http://www.example.com:8080 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google\Chrome\InsecurePrivateNetworkRequestsAllowedForUrls" /v 2 /t REG_SZ /d [*.]example.com /f
 
-#EDGE
+# EDGE
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge" /v InsecurePrivateNetworkRequestsAllowed /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge\InsecurePrivateNetworkRequestsAllowedForUrls" /v 1 /t REG_SZ /d  http://www.example.com:8080 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Edge\InsecurePrivateNetworkRequestsAllowedForUrls" /v 2 /t REG_SZ /d [*.]example.com /f
 
-#BRAVE
+# BRAVE
 reg add "HKEY_LOCAL_MACHINE\Software\Policies\BraveSoftware\Brave" /v InsecurePrivateNetworkRequestsAllowed /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\Software\Policies\BraveSoftware\Brave\InsecurePrivateNetworkRequestsAllowedForUrls" /v 1 /t REG_SZ /d  http://www.example.com:8080 /f
 reg add "HKEY_LOCAL_MACHINE\Software\Policies\BraveSoftware\Brave\InsecurePrivateNetworkRequestsAllowedForUrls" /v 2 /t REG_SZ /d [*.]example.com /f
