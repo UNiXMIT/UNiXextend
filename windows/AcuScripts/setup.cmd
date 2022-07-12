19c3e15e8b42da0be27accca09e5038b064fb9a2
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-07-12 11:25:40 +0100

    Updates

--- windows/AcuScripts/setup.cmd
+++ windows/AcuScripts/setup.cmd
@@ -102,6 +102,6 @@ reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" /
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
 
 :: Show file extensions
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFilesExt /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFilesExt /t REG_DWORD /d 0 /f
 
 cd \temp
