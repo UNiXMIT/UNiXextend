bcff2acb0cca13f324ef465d579809bf47a01e23
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-07-25 13:07:00 +0100

    Updates

--- linux/etc/fillCombo.js
+++ linux/etc/fillCombo.js
@@ -2,6 +2,6 @@
 {"type": "category", "text": "Sample Applications"},
 {"type": "elem", "text": "Vision", "value": "Vision"},
 {"type": "elem", "text": "Tour", "value": "Tour"},
-{"type": "elem", "text": "ATWD", "value": "AcuToWeb Desktop Check"},
+{"type": "elem", "text": "AcuToWeb Desktop Check", "value": "ATWD"},
 {"type": "category", "text": "Customer Applications"}
 ]
--- windows/etc/fillCombo.js
+++ windows/etc/fillCombo.js
@@ -1,8 +1,8 @@
 [
-    {"type": "category", "text": "Sample Applications"},
-    {"type": "elem", "text": "Vision", "value": "Vision"},
-    {"type": "elem", "text": "Tour", "value": "Tour"},
-    {"type": "elem", "text": "ATWD", "value": "AcuToWeb Desktop Check"},
-    {"type": "category", "text": "Customer Applications"}
-    ]
+{"type": "category", "text": "Sample Applications"},
+{"type": "elem", "text": "Vision", "value": "Vision"},
+{"type": "elem", "text": "Tour", "value": "Tour"},
+{"type": "elem", "text": "AcuToWeb Desktop Check", "value": "ATWD"},
+{"type": "category", "text": "Customer Applications"}
+]
     
