09d0ec91607fff640a71fd616a4fbf9737ee06d5
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-08-04 11:47:05 +0100

    Updates

--- linux/AcuScripts/wsl2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo ip addr flush dev eth0
-sudo ip addr add 10.0.0.254/24 dev eth0
-sudo ip link set eth0 up
-sudo ip route add default via 10.0.0.1 dev eth0
-sudo echo nameserver 1.1.1.1 > /etc/resolv.conf
-sudo echo [network] > /etc/wsl.conf
-sudo echo generateResolvConf = false >> /etc/wsl.conf
