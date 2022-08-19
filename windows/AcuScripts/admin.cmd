b604faddaf631b3a814555712db22d465328b8ce
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2022-08-19 10:18:25 +0100

    Admin for AcuServe.exe

--- windows/AcuScripts/admin.cmd
+++ windows/AcuScripts/admin.cmd
@@ -13,6 +13,12 @@ GOTO ATW
 reg query "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Micro Focus\extend %EXTEND%\AcuGT\bin\AcuToWeb.exe" >nul 2>&1
 IF %ERRORLEVEL% == 0 GOTO END
 reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Micro Focus\extend %EXTEND%\AcuGT\bin\AcuToWeb.exe" /t REG_SZ /d "~ RUNASADMIN" /f
+GOTO ACUSERVE
+
+:ACUSERVE
+reg query "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Micro Focus\extend %EXTEND%\AcuGT\bin\acuserve.exe" >nul 2>&1
+IF %ERRORLEVEL% == 0 GOTO ATW
+reg add "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Layers" /v "C:\Program Files (x86)\Micro Focus\extend %EXTEND%\AcuGT\bin\acuserve.exe" /t REG_SZ /d "~ RUNASADMIN" /f
 GOTO END
 
 :END
