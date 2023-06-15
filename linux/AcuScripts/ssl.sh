6759f6f663cd41ee440d7d803df5ee600d217c51
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-06-15 13:47:14 +0100

    Re-format SSL command

--- linux/AcuScripts/ssl.sh
+++ linux/AcuScripts/ssl.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# Generate server certificate and key
-openssl req -x509 -out server.crt -keyout server.key -newkey rsa:2048 -nodes -sha256 -subj '/CN=localhost' -extensions EXT -config <(printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
+# Generate a private key and self-signed certificate for localhost
+openssl req -x509 -out server.crt -keyout server.key \
+  -newkey rsa:2048 -nodes -sha256 \
+  -subj '/CN=localhost' -extensions EXT -config <( \
+   printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
