694257ddef814304acff988fdfffac2f4420c41d
UNiXMIT <4117073+UNiXMIT@users.noreply.github.com>
2023-09-29 15:14:09 +0100

    Updates

--- linux/AcuScripts/setup.sh
+++ linux/AcuScripts/setup.sh
@@ -19,9 +19,9 @@ if command -v dnf >/dev/null; then
   . /etc/os-release;
   sudo dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${VERSION_ID%.*}.noarch.rpm;
   if [[ ${VERSION_ID%.*} -le 8 ]]; then
-    sudo dnf install -y libnsl podman buildah unixODBC wget curl cronie dos2unix java-11-openjdk htop tmux libstdc++.i686 libxcrypt.i686 ncurses-libs-6.1-9.20180224.el8.i686 libaio-devel glibc.i686 zlib-1.2.11-18.el8_5.i686 tcpdump ed glibc-devel.i686 spax;
+    sudo dnf install -y --skip-broken libnsl podman buildah unixODBC wget curl cronie dos2unix java-11-openjdk htop tmux libstdc++.i686 libxcrypt.i686 ncurses-libs-6.1-9.20180224.el8.i686 libaio-devel glibc.i686 zlib-1.2.11-18.el8_5.i686 tcpdump ed glibc-devel.i686 spax;
   elif [[ ${VERSION_ID%.*} -ge 9 ]]; then
-    sudo dnf install -y libnsl podman buildah unixODBC wget curl cronie dos2unix java-11-openjdk htop tmux libstdc++.i686 libxcrypt.i686 ncurses-libs-6.1-9.20180224.el8.i686 libaio-devel glibc.i686 zlib-1.2.11-18.el8_5.i686 tcpdump ed glibc-devel.i686;
+    sudo dnf install -y --skip-broken libnsl podman buildah unixODBC wget curl cronie dos2unix java-11-openjdk htop tmux libstdc++.i686 libxcrypt.i686 ncurses-libs-6.1-9.20180224.el8.i686 libaio-devel glibc.i686 zlib-1.2.11-18.el8_5.i686 tcpdump ed glibc-devel.i686;
   fi
     sudo setenforce 0;
     sudo sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config;
@@ -30,7 +30,7 @@ elif command -v apt >/dev/null; then
   sudo apt update;
   sudo apt upgrade -y;
   sudo dpkg --add-architecture i386;
-  sudo apt install -y build-essential podman buildah unixodbc-dev wget curl cron dos2unix default-jdk htop tmux lib32stdc++6 libaio-dev libncurses5 apt-file zlib1g:i386 tcpdump libgc1 pax;
+  sudo apt install -y -m build-essential podman buildah unixodbc-dev wget curl cron dos2unix default-jdk htop tmux lib32stdc++6 libaio-dev libncurses5 apt-file zlib1g:i386 tcpdump libgc1 pax;
   sudo apt-file update;
 elif command -v zypper >/dev/null; then
   sudo zypper refresh;
